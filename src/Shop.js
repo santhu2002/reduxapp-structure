e61d763dbd5933f9b33303f533468197cc18b622
Santhu <purisanthosh2002@gmail.com>
2022-01-25 12:39:27 +0530

    Redux project setup

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Shop = () => {
+  return(
+      <div className="container my-2">
+          <h2>Shop here to get extra discounts</h2>
+          <button className="btn btn-primary mx-2">-</button>
+          Add to cart
+          <button className="btn btn-primary mx-2">+</button>
+      </div>
+  );
+};
+
+export default Shop;
